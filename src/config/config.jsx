e5ebafdbc8cd6a67185222a1c06f4888b469657c
Abhay <agoyal4_be19@thapar.edu>
2023-11-06 23:16:11 +0530

    Update config.jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,6 @@
 const config = {
   // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://moengage-assignments-backend.vercel.app/',
+  baseUrl: 'https://moengage-assignment-backend-68fc.vercel.app/',
 };
 
 export default config;
